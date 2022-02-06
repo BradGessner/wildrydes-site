c7043d1239e7d1795d910ed90a4a4e47612f5043
Brad Gessner <bradgessner@nexstage.com>
2022-02-06 14:48:01 -0800

    Add files via upload
    On branch master
    Changes to be committed:
    modified:   js/config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_GyRtrnjv5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4u54njrbbsacf1q5lg53g0aab4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv 
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
